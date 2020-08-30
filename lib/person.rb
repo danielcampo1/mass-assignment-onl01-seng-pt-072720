d5412d5df26a08da0c27a34fe54d3edca76644aa
daniel campo <campo_daniel94@yahoo.com>
2020-08-30 03:39:17 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -19,7 +19,9 @@ wrist_size:, glove_size:, pant_length:, pant_width: )
     @pant_length = pant_length
     @pant_width = pant_width
  end
- spender.send ("name=", "name")
+# spender.send ("name=", "name")
  
   
 end
+
+spender.send ("name=", "name")
