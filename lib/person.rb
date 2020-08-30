130516b27a6bb6ed05b223754e25ee3035ee1f24
daniel campo <campo_daniel94@yahoo.com>
2020-08-30 03:52:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,21 +4,9 @@ class Person
     :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize (attributes)
-attributes.each {|key, value| self.send(("#{key}="), value)}
-    # @name = name 
-    # @birthday = birthday
-    # @hair_color = hair_color
-    # @eye_color = eye_color
-    # @height = height
-    # @weight = weight
-    # @handed = handed
-    # @complexion = complexion
-    # @t_shirt_size = t_shirt_size
-    # @wrist_size = wrist_size
-    # @glove_size = glove_size
-    # @pant_length = pant_length
-    # @pant_width = pant_width
- end
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+   
+  end
 
  
   
