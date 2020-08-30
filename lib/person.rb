e446369e7f6debc3c226122a67ad556fd57fcdc7
daniel campo <campo_daniel94@yahoo.com>
2020-08-30 03:41:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
   
   def initialize (name:, birthday:, hair_color:, eye_color:, height:,weight:, handed:, complexion:, t_shirt_size:, 
 wrist_size:, glove_size:, pant_length:, pant_width: )
-    @name = name 
+   avi_ attributes ={ @name = name 
     @birthday = birthday
     @hair_color = hair_color
     @eye_color = eye_color
@@ -17,9 +17,9 @@ wrist_size:, glove_size:, pant_length:, pant_width: )
     @wrist_size = wrist_size
     @glove_size = glove_size
     @pant_length = pant_length
-    @pant_width = pant_width
+    @pant_width = pant_width}
  end
-spender.send ("name=", "name")
+
  
   
 end
